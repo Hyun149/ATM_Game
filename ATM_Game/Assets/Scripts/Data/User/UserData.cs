51376188c46b335e36bf0477e28bf182c18fb39a
Hyun149 <rusia4527@Gmail.com>
2025-06-06 18:43:54 +0900

    docs: UserData 및 UserDataList 클래스에 XML 주석 추가
    - UserData 클래스에 클래스 설명 및 생성자/메서드 주석 추가
    - UserDataList 클래스에 전체 설명 및 생성자 주석 작성
    - 직렬화 목적 및 사용 맥락을 명확히 기술

--- ATM_Game/Assets/Scripts/Data/User/UserData.cs
+++ ATM_Game/Assets/Scripts/Data/User/UserData.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 유저의 정보를 저장하는 데이터 클래스입니다.
+/// - 이름, 아이디, 비밀번호, 보유 현금/잔액 등을 포함합니다.
+/// - 인벤토리 정보도 함께 직렬화할 수 있습니다.
+/// </summary>
 [System.Serializable]
 public class UserData
 {
@@ -13,6 +18,14 @@ public class UserData
     
     public List<ItemSaveData> inventory = new List<ItemSaveData>();
 
+    /// <summary>
+    /// UserData 인스턴스를 생성합니다.
+    /// </summary>
+    /// <param name="userName">유저 이름</param>
+    /// <param name="id">유저 ID</param>
+    /// <param name="pw">유저 비밀번호</param>
+    /// <param name="cash">보유 현금</param>
+    /// <param name="balance">보유 예치금</param
     public UserData(string userName, string id, string pw,int cash, int balance)
     {
         this.userName = userName;
@@ -22,6 +35,11 @@ public class UserData
         this.password = pw;
     }
 
+    /// <summary>
+    /// 캐릭터 데이터를 기반으로 현재 유저의 인벤토리 정보를 저장합니다.
+    /// - 기존 인벤토리를 클리어하고, 장착 여부와 함께 아이템 정보를 기록합니다.
+    /// </summary>
+    /// <param name="character">저장할 대상 캐릭터 객체</param>
     public void SaveFromCharacter(Character character)
     {
         this.inventory.Clear();
--- ATM_Game/Assets/Scripts/Data/User/UserDataList.cs
+++ ATM_Game/Assets/Scripts/Data/User/UserDataList.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 전체 유저 정보를 저장하는 리스트 클래스입니다.<br/>
+/// - 여러 유저의 정보를 JSON으로 저장/불러오기 위해 사용됩니다.
+/// </summary>
 [System.Serializable]
 public class UserDataList
 {
     public List<UserData> users;
 
+    /// <summary>
+    /// 빈 유저 리스트를 생성합니다.
+    /// </summary>
     public UserDataList()
     {
         users = new List<UserData>();
